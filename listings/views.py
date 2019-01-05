98d1d69654fa93f7acb375b406fb698c5dfeb670
Ayelegun Kayode Michael <billmike1994@gmail.com>
2019-01-05 23:28:23 +0100

    Fetch listings data from DB and display in view Fetch realtors data from DB and display in view

--- listings/views.py
+++ listings/views.py
@@ -1,10 +1,24 @@
 from django.shortcuts import render
+from django.core.paginator import PageNotAnInteger, EmptyPage, Paginator
+
+from .models import Listing
 
 # Create your views here.
 def listings(request):
-  return render(request, 'listings/listings.html')
+  all_listings = Listing.objects.order_by('-list_date').filter(is_published=True)
 
-def listing(request):
+  paginator = Paginator(all_listings, 6)
+
+  page = request.GET.get('page')
+
+  paged_listing = paginator.get_page(page)
+
+  context = {
+    'all_listings': paged_listing
+  }
+  return render(request, 'listings/listings.html', context)
+
+def listing(request, listing_id):
   return render(request, 'listings/listing.html')
 
 def search(request):
